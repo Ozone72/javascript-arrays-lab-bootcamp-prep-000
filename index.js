992934800e8d86b26842e34b5bd952a2056f3d22
Orin Fletcher <orin.fletcher@gmail.com>
2017-09-22 19:25:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = "I don't do much."
 
 var kittens = ['Milo', 'Otis', 'Garfield']
+console.log(`Starting array: ${kittens}`)
 function destructivelyAppendKitten(name){
   kittens.push(name)
   console.log(kittens)
